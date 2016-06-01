1afcfbf82468198444a250a0a8c5c084f64ca67e
Dmitriy Gruzdev <Dmitriy_Gruzdev@epam.com>
2016-06-01 14:55:40 +0300

    --/items create

---
+++ src/main/webapp/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rambleApp
+ * @description
+ * # rambleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rambleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ src/main/webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rambleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rambleApp
+ */
+angular.module('rambleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
