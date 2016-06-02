0739cfe268da0d8c84b8788e1424cf2ac6f566df
Dmitriy Gruzdev <Dmitriy_Gruzdev@epam.com>
2016-06-02 23:46:48 +0300

    --cart and close cart

--- src/main/webapp/app/scripts/services/account.js
+++ src/main/webapp/app/scripts/services/account.js
@@ -10,8 +10,14 @@
 angular.module('rambleApp')
   .factory('account', function (Network) {
     return {
+      getCartPositions: function (onSuccess, onError) {
+        return Network.sendGet("/account/cart/positions", {}, onSuccess, onError);
+      },
       getCart: function (onSuccess, onError) {
         return Network.sendGet("/account/cart", {}, onSuccess, onError);
+      },
+      closeCart: function (onSuccess, onError) {
+        return Network.sendPut("/account/cart", {}, onSuccess, onError);
       }
     };
   });
